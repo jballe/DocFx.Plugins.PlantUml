745f2b9559e1da8482699913b0d0127056ee3f43
Jesper Balle <Jesper.Balle@valtech.com>
2019-11-13 09:43:12 +0100

    Attempt to set workingDir for current diagram file

--- src/PlantUmlMarkdownRenderer.cs
+++ src/PlantUmlMarkdownRenderer.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.DocAsCode.Dfm;
 using Microsoft.DocAsCode.MarkdownLite;
 using PlantUml.Net;
@@ -29,11 +32,39 @@ namespace DocFx.Plugins.PlantUml
 
         public override StringBuffer Render(IMarkdownRenderer markdownRenderer, MarkdownCodeBlockToken token, MarkdownBlockContext context)
         {
-            IPlantUmlRenderer plantUmlRenderer = rendererFactory.CreateRenderer(settings);
+            //var dir = Path.Combine(context.GetBaseFolder(), GetFilePath(context));
+            //var dir = Path.Combine(context.GetBaseFolder(), context.GetFilePathStack().Peek());
+            var dir = Path.Combine(context.GetBaseFolder(), "diagrams");
+            IPlantUmlRenderer plantUmlRenderer = rendererFactory.CreateRenderer(settings, dir);
             IOutputFormatter outputFormatter = formatterFactory.CreateOutputFormatter(markdownRenderer.Options);
+            File.WriteAllText(@"c:\out\workingdir.log", dir);
 
-            byte[] output = plantUmlRenderer.Render(token.Code, settings.OutputFormat);
+            byte[] output;
+            if (outputFormatter.UseUri)
+            {
+                var uri = plantUmlRenderer.RenderAsUri(token.Code, settings.OutputFormat);
+                output = System.Text.Encoding.UTF8.GetBytes(uri.ToString());
+            }
+            else
+            {
+                output = plantUmlRenderer.Render(token.Code, settings.OutputFormat);
+            }
+
             return outputFormatter.FormatOutput(token, output);
         }
+
+        // private string GetFilePath(MarkdownBlockContext context)
+        // {
+        //     // Will use reflection as a got assembly version error for System.Collections.Immutable
+        //     var stack = context.Variables["FilePathStack"];
+        //     var ie = (IEnumerable<string>) stack;
+        //     return ie.First();
+        // 
+        // 
+        //     var t = stack.GetType();
+        //     var peekMethod = t.GetMethod("Peek");
+        //     var result = peekMethod.Invoke(stack, null) as string;
+        //     return result;
+        // }
     }
 }
